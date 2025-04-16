ed6d96491a748534b9be20bfec09eb21b43db335
OsmairS3t <osmair144@gmail.com>
2025-04-16 15:58:57 -0300

    transaction included

--- src/utils/interface.ts
+++ src/utils/interface.ts
@@ -73,3 +73,12 @@ export interface IReservation {
   amount: number;
   commandnumber: number;
 }
+
+export interface ITransaction {
+  id: number;
+  data: Date;
+  tipo: string;
+  descricao: string;
+  quant: string;
+  valor: number;
+}
